536b613ae4b26572ccdf86fcf251e6935d01f3ca
MY RAILS <jhambrohan44@gmail.com>
2017-09-24 04:27:53 +0530

    nameCalculatorApp  using angularjs

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
-(function (){
+//-----------------index.html----------------
+// (function (){
 
-	'use strict'
+// 	'use strict'
 
-	angular.module('myFirstApp' , [])
+// 	angular.module('myFirstApp' , [])
 
-			.controller('myFirstController' , function($scope){
-				$scope.name = "rohan";
-				$scope.sayHello = function(){
-					return "hey hello how are you . angularjs i am coming .be careful you bitch";
-				};
-			});
+// 			.controller('myFirstController' , function($scope){
+// 				$scope.name = "rohan";
+// 				$scope.sayHello = function(){
+// 					return "hey hello how are you . angularjs i am coming .be careful you bitch";
+// 				};
+// 			});
 
-})();
+// })();
 
 //iifs helps us from bleeding things to global scope
+
+//----------------nameCalculator.html----------------
+
+(function(){
+	'use strict'
+
+	angular.module('nameCalculatoraApp' , [])
+
+			.controller('nameCalculatorController' , function($scope){
+				$scope.name = "";
+				$scope.totalValue = 0;
+				
+				$scope.displayNumberic = function(){
+					var totalValueNumber =calculateValue($scope.name);
+
+					
+					$scope.totalValue = totalValueNumber;
+				};
+
+				function calculateValue(string){
+					var totalStirngValue = 0 ;
+					for (var i = 0; i < string.length; i++) {
+						totalStirngValue += string.charCodeAt(i);
+					}
+
+					return totalStirngValue ;
+				};
+
+			});
+})();
